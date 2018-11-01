fa097272aa9ef89aa8ec6e6f60fee6d8d998fac2
Mohamed Elhachmi <elhachmi@gmail.com>
2018-11-01 14:05:04 +0000

    launch deamons via autostart

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-
-fixApiWriteAccess() {
-    chown -R www-data:www-data /pipeline/source/var
-    chown -R www-data:www-data /pipeline/source/web/storage
-    chown -R www-data:www-data /pipeline/source/app/db
-}
-
 if [ $1 = "api" ]
 then
     if [ -z $2 ]
@@ -15,17 +8,13 @@ then
     else
         env=$2
     fi
-    echo "php7 contianer launche daemons"
-    /usr/local/bin/php /pipeline/source/bin/console doctrine:schema:update -f  --env=$env >  /tmp/doctrine
-    fixApiWriteAccess
-    su -s /bin/sh -c "/usr/bin/nohup /usr/local/bin/php /pipeline/source/bin/console ttpmailer:daemon:start --env=$env </dev/null >/dev/null 2>&1 &" www-data
-    su -s /bin/sh -c "/usr/bin/nohup /usr/local/bin/php /pipeline/source/bin/console ttp:dir:push:daemon:start --sleep=2 --env=$env </dev/null >/dev/null 2>&1 &" www-data
-    su -s /bin/sh -c "/usr/bin/nohup /usr/local/bin/php /pipeline/source/bin/console ttp:dir:transfer-queue —env=$env </dev/null >/dev/null 2>&1 &" www-data
-    su -s /bin/sh -c "/usr/bin/nohup /usr/local/bin/php /pipeline/source/bin/console ttp:api:user-export-queue --env=$env --no-debug </dev/null >/dev/null 2>&1 &" www-data
+    echo "php7 container launche daemons"
+    su -s /bin/sh -c '/usr/local/bin/php /pipeline/source/bin/console doctrine:schema:update -f  --env=$env >  /tmp/doctrine' www-data
     (crontab -l 2>/dev/null; echo "* * * * * /pipeline/source/ecs/autostart.sh $env") | crontab -
     (crontab -l 2>/dev/null; echo "0 23 * * * /pipeline/source/ecs/validate_channel_configs.sh $env") | crontab -
     (crontab -l 2>/dev/null; echo "* * * * * su -s /bin/sh -c '/usr/local/bin/php /pipeline/source/bin/console ttp:blog:publish-scheduled-articles --env=$env > /tmp/schedule-article.log' www-data") | crontab -
     service cron start
+    /pipeline/source/ecs/autostart.sh $env
     tail -f /dev/null
 else
    php-fpm
