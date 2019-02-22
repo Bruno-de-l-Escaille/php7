e3cecdffd4d2707d10686b9bc1155bc16b158df8
elhachmi <elhachmi@gmail.com>
2019-02-22 13:43:39 +0000

    Set pam session cron

--- 7.2/cli/entrypoint.sh
+++ 7.2/cli/entrypoint.sh
@@ -5,11 +5,13 @@ if [ -z "$1" ]
 else
       env=$1
 fi
-echo "php7 container launche daemons"
+sed -i '/session.*required.*pam_loginuid.so/s/session/#session/g' /etc/pam.d/cron
 su -s /bin/sh -c "/usr/local/bin/php /pipeline/source/bin/console doctrine:schema:update -f  --env=$env >  /tmp/doctrine" www-data
+/pipeline/source/ecs/autostart.sh $env
 (crontab -l 2>/dev/null; echo "* * * * * /pipeline/source/ecs/autostart.sh $env") | crontab -
 (crontab -l 2>/dev/null; echo "0 23 * * * /pipeline/source/ecs/validate_channel_configs.sh $env") | crontab -
 (crontab -l 2>/dev/null; echo "* * * * * su -s /bin/sh -c '/usr/local/bin/php /pipeline/source/bin/console ttp:blog:publish-scheduled-articles --env=$env > /tmp/schedule-article.log' www-data") | crontab -
 service cron start
-#/pipeline/source/ecs/autostart.sh $env
+echo "cron job started"
+echo "php7 container launche daemons"
 tail -f /dev/null
